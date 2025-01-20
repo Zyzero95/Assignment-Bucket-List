69625ac50eaf4a44da82fc093a67c3405e6f7e38
Viktor Lofgren <johan.viktor.lofgren@live.com>
2025-01-20 09:56:43 +0100

    Added more comments.

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 // Empty array for Bucket List.
 let bucketListItems = [];
 
-// Creating DOM Selectors
+// Creating DOM Selectors.
 const bucketlistSelector = document.querySelector("#bucketListing");
 const btnSubmit = document.querySelector("#buttonSubmit");
 
-// Button Event Listener
+// Button Event Listener.
 btnSubmit.addEventListener("click", (event) => {
 
+    // preventDefault() prevents page from refreshing when using submit button.
     event.preventDefault();
     submitBucketListItem();
 });
 
-// Add Input text and option value to array(bucketListItems)
+// Add Input text and option value to array(bucketListItems).
 function submitBucketListItem() {
 
-    // Create DOM Selectors of value when button is clicked
+    // Create DOM Selectors of value when button is clicked.
     const activityName = document.querySelector("#activityName").value.trim();
     const activityCategory = document.querySelector("#activityCategory").value;
 
-    // Empty object for submit
+    // Empty object for submit.
     let objHolder = {
     Activity: "",
     Category: ""
     };
 
-    // Assign values to object
+    // Assign values to object.
     objHolder.Activity = activityName;
     objHolder.Category = activityCategory;
 
-    // renderBucketList();
     addItem(objHolder);
 }
 
 function addItem(listItem) {
-    // Add object to array
+    // Add object to array.
     bucketListItems.push(listItem);
 
+    // This code section uses DOM to create HTML elements for the bucket list.
     let listData = document.createElement('li');
 
     let listActivityText = document.createElement('p');
@@ -63,13 +64,11 @@ function addItem(listItem) {
     removeButton.innerHTML = "Remove";
     listData.appendChild(removeButton);
 
+    // Add the HTML elements as children to the Bucket List Section.
     bucketlistSelector.appendChild(listData);
-
-    console.log(bucketListItems);
-    console.log(document.querySelector('ul#bucketListing').children);
 }
 
-// Event Delegation searching only for dynamically created buttons
+// Event Delegation searching for dynamically created buttons.
 bucketlistSelector.addEventListener("click" , (event) => {
     if(event.target.classList.contains('remove-button')){
         removeItem(event.target);
@@ -82,9 +81,11 @@ bucketlistSelector.addEventListener("click" , (event) => {
 // We change the text inside each p tag with the values from the form.
 function editItem(e){
 
+    // Get value from Input text and Select value.
     const activityName = document.querySelector("#activityName").value.trim();
     const activityCategory = document.querySelector("#activityCategory").value;
 
+    // Change value of the two paragraphs we have created which will always be set on index 0 and 1.
     e.parentElement.children[0].innerHTML = activityName;
     e.parentElement.children[1].innerHTML = activityCategory;
 }
